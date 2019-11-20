7dbc74f21633a9340df54a822f6bfc037819f8bf
Trinkle23897 <463003665@qq.com>
2019-11-20 08:50:21 +0800

    init

---
+++ ethz.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+username = 'test@gmail.com'
+password = 'test'
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    # login
+    driver.get("https://www.lehrbetrieb.ethz.ch/eApply/ealogin.view?lang=en")
+    driver.find_element_by_name("ea_mail").send_keys(username)
+    driver.find_element_by_name("ea_password").send_keys(password)
+    driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forgot your password?'])[1]/following::input[1]").click()
+    driver.get("https://www.lehrbetrieb.ethz.ch/eApply/overview.view?indx=0")
+    driver.find_element_by_link_text("Courses").click()
+    # load courses
+    raw = open('course.txt').read().split('\n')[:-1]
+    for s in raw:
+        print(s)
+        c = s.split('\t')
+        driver.find_element_by_id("add").click()
+        Select(driver.find_element_by_id("subjectCategory")).select_by_visible_text("Course")
+        driver.find_element_by_id("subjectCategory").click()
+        driver.find_element_by_id("title").send_keys(c[1])
+        driver.find_element_by_id("academicYear").send_keys(c[5])
+        driver.find_element_by_id("courseNumber").send_keys(c[0])
+        driver.find_element_by_id("weeks").send_keys(c[9])
+        driver.find_element_by_id("hoursTutorial").send_keys(c[6])
+        driver.find_element_by_id("hoursLectures").send_keys(c[7])
+        driver.find_element_by_id("hoursPracticalWork").send_keys(c[8])
+        driver.find_element_by_id("grade").send_keys(c[3])
+        driver.find_element_by_id("credits").send_keys(c[2])
+        driver.find_element_by_id("store").click()
---
+++ readme.md
@@ -0,0 +1,42 @@
+# ETHz courses auto upload
+
+n+e
+
+## Dependency
+
+Python3, selenium
+
+For installing selenium, you can refer to [here](https://morvanzhou.github.io/tutorials/data-manipulation/scraping/5-01-selenium/)
+
+ps. I test the script in Chrome driver. If you use Firefox, please replace the `Chrome` in script.
+
+## Usage
+
+1. Replace your username and password in `ethz.py`;
+
+2. Add your courses into `course.txt`. For Tsinghua students, it can directly copy the courses from English transcript page in [info](http://zhjw.cic.tsinghua.edu.cn/cj.cjCjbAll.do?m=bks_cjdcx&cjdlx=yw), then add the last 4 terms;
+
+3. `python3 ethz.py`;
+
+4. Have a cup of tea.
+
+## Format
+
+The format in `course.txt` is as follows (in a single line, separated by Tab):
+
+- Course ID (12090043)
+- Course Name (Military Theory and Skill Training)
+- Credit (3)
+- Grade (B+)
+- Points (3.6)
+- Year (2016)
+- hours Tutorial (0)
+- hours Lectures (8)
+- hours Practical Work (40)
+- Weeks (3)
+
+which results in: `12090043	Military Theory and Skill Training	3	B+	3.6	2016	0	8	40	3`
+
+## What does it look like?
+
+<video src="http://ml.cs.tsinghua.edu.cn/~jiayi/video/ethz.mov" controls="controls"> 您的浏览器不支持video 标签。 </video>
